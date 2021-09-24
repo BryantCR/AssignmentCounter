7570036bf9862eb82b2a6cf4a61f007fd29e92d6
BryantCR <bryancvargas20@gmail.com>
2021-09-24 16:25:53 -0600

    all working good

--- counter.py
+++ counter.py
@@ -1,12 +1,23 @@
-from flask import flask
+from flask import Flask
 from flask import render_template
+from flask import session
+from flask import redirect
 
 app = Flask( __name__ )
+app.secret_key = "secret"
 
+@app.route( '/', methods=['GET'] )
+def viewsCounter():
+    if "counter" not in session:
+        session["counter"] = 0
+    else:
+        session["counter"] += 1
+    return render_template('index.html')
 
-
-
-
+@app.route( '/destroy_session', methods=['GET'] )
+def closeSession():
+    session.clear()
+    return redirect( '/' )
 
 if __name__ == "__main__":
     app.run( debug = True )
